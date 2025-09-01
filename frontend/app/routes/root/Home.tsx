b046bca0c980d8f62548b4288805cca8062f4726
CharlesOsang017 <charlesosango02@gmail.com>
2025-09-01 21:46:44 +0300

    Add a home page display

--- frontend/app/routes/root/Home.tsx
+++ frontend/app/routes/root/Home.tsx
@@ -1,9 +1,20 @@
-const Home = () => {
-  return (
-    <div>    
-      <h1>Home</h1>
-    </div>
-  )
-}
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
 
-export default Home
+export const Home = () => {
+  return (
+    <div className='w-full h-screen flex items-center justify-center gap-4'>
+      <h2 className='text-2xl mb-2 text-semibold'>
+        Authentication Build with MERN
+      </h2>
+      <Link to='/sign-in'>
+        <Button className='bg-blue-500 text-white'>Login</Button>
+      </Link>
+      <Link to='/sign-up'>
+        <Button variant={"outline"} className='bg-blue-500 text-white'>
+          Sign Up
+        </Button>
+      </Link>
+    </div>
+  );
+};
